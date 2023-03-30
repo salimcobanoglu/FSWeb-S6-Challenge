6fc43eba1a7d716cf637526eee9e9e78c5e8ceda
Salim Çobanoğlu <salimcobanoglu@yahoo.com>
2023-03-30 23:41:50 +0200

    ilk adimlar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
-import React from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Karakter from "./components/Karakter";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [starwars, setStarwars] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people/", {})
+      .then(function (response) {
+        console.log(response.data);
+        setStarwars(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">Karakterler</h1>
+      <div>
+        {starwars.map((x) => (
+          <Karakter starwars={x} />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -1,1 +1,7 @@
 // Karakter bileşeniniz buraya gelecek
+
+export default function Karakter(props) {
+  const { data } = props;
+
+  return <div>{data.name}</div>;
+}
